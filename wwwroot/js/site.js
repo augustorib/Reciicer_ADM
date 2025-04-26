207c25d4500a7a7d5e044884ff9dd682b8169830
augustorib <augustorifra@gmail.com>
2025-04-26 05:06:07 -0300

    feat: data table responsive

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,29 +1,10 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-
-//Carregar dinamicamente select da tela "View/ReciclagemCreate"
-// document.getElementById('carregarSelects').addEventListener('click', function () {
-//     var qtdMateriais = document.getElementById('QtdMateriais').value;
-
-//     $.ajax({
-//         url: 'CarregarSelectMateriais',
-//         type: 'GET',
-//         data: {count : qtdMateriais},
-//         success: function (response) {
-//              $('#selectMateriais').html(response);
-//         },
-//         error: function (xhr, status, error) {
-//             console.error('Erro ao carregar materiais: ', error);
-//         }
-//     });
-// });
-
-//Drop down em cascata Coleta/Create
 $(document).ready(function (){  
 
     $('#dataTableInit').DataTable({
+      
       "language": {
                     "sEmptyTable": "Nenhum dado disponível na tabela",
                     "sInfo": "Mostrando _START_ até _END_ de _TOTAL_ registros",
@@ -47,7 +28,8 @@ $(document).ready(function (){
                         "sSortDescending": ": Ordenar colunas de forma descendente"
                     },
                 },
-        "order": []
+        "order": [],
+        "responsive": true,
       });
 
 
